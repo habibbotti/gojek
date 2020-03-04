d32e019bac46a10694b3b9023ee68fb9522ca107
habibbotti <39511299+habibbotti@users.noreply.github.com>
2020-03-04 16:36:59 +0700

    Update func.php

--- func.php
+++ func.php
@@ -241,7 +241,7 @@ function claims($token,$voc)
 
 function reff($token)
     {
-    $data = '{"referral_code":"G-DR8NNZT"}';    
+    $data = '{"referral_code":"G-MPW4WBM"}';    
     $claim = request("/customer_referrals/v1/campaign/enrolment", $token, $data);
     if ($claim['success'] == 1)
         {
